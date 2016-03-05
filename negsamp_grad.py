62aad60a593a699a66cd871e7087bfb7d78e7203
d10genes <wcbeard10@gmail.com>
2016-03-05 00:00:09 -0500

    jit -> nopython

--- negsamp_grad.py
+++ negsamp_grad.py
@@ -1,13 +1,13 @@
-from numba import jit as jit_
+from numba import jit
 import numpy as np
 
-jit = jit_(nopython=1)
-getNall = jit(lambda W: W.shape[1] // 2)
-sig = jit(lambda x: 1 / (1 + np.exp(-x)))
-gen_labels = jit(lambda pos_negs: [1] + [0] * (len(pos_negs) - 1))
+nopython = jit(nopython=1)
+getNall = nopython(lambda W: W.shape[1] // 2)
+sig = nopython(lambda x: 1 / (1 + np.exp(-x)))
+gen_labels = nopython(lambda pos_negs: [1] + [0] * (len(pos_negs) - 1))
 
 
-@jit
+@nopython
 def dot(a, b):
     sm = 0
     for i in range(len(a)):
@@ -15,7 +15,7 @@ def dot(a, b):
     return sm
 
 
-@jit
+@nopython
 def get_vecs1(Wsub):
     """Extract weight vectors from subset of
     negative-sampling skip-gram weight matrix.
@@ -28,18 +28,18 @@ def get_vecs1(Wsub):
     return h, vwo_negsamps
 
 
-@jit
+@nopython
 def ns_prob(h=None, vout=None, label=None):
     return sig(dot(vout, h)) - label
 
 
-@jit
+@nopython
 def ns_loss_grads(h, vout, label):
     dotprod = ns_prob(h=h, vout=vout, label=label)
     return dotprod * vout, dotprod * h
 
 
-@jit
+@nopython
 def inner(Wsub_grad, N, h, i, vout, label):
     hgrad, vgrad = ns_loss_grads(h, vout, label)
     for j in range(N):
@@ -47,13 +47,13 @@ def inner(Wsub_grad, N, h, i, vout, label):
         Wsub_grad[i, N + j] += vgrad[j]
 
 
-@jit
+@nopython
 def loop(Wsub_grad, N, h, vwo_negsamps):
     for i, label in enumerate(gen_labels(vwo_negsamps), 1):
         inner(Wsub_grad, N, h, i, vwo_negsamps[i - 1], label)
 
 
-@jit
+@nopython
 def ns_grad(Wsub):
     h, vwo_negsamps = get_vecs1(Wsub)
     N = getNall(Wsub)
